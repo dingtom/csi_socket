1ab8708385bf903a852b0636d3b0f39738f777d9
VanderChen <vanderchen@outlook.com>
2018-09-15 13:47:49 +0800

    change server

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
-import socket  
-  
+import socket
+import struct
+from read_bf_file import read_bfee
+
 address = ('0.0.0.0', 8887)  
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # s = socket.socket()  
 s.bind(address)  
@@ -10,10 +12,12 @@ s.listen(5)
 ss, addr = s.accept()  
 print('got connected from',addr)
 while(True):
-    ra = ss.recv(4096)  
+    ra = ss.recv(8192)  
     # print(ra)
     if ra != b'':
-        print(ra)
+        if ra[0] == 187:
+            csi = read_bfee(ra[1:])
+            print(csi.timestamp_low)
     else:
         break
   
